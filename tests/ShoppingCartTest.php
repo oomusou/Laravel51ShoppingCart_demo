691e36233bf69455c26920801068f46068abae8c
oomusou <oomusou@gmail.com>
2015-10-29 10:02:36 +0800

    第一集買了一本，第二集也買了一本，價格應為100*2*0.95=190 紅燈

--- tests/ShoppingCartTest.php
+++ tests/ShoppingCartTest.php
@@ -46,4 +46,30 @@ class ShoppingCartTest extends TestCase
         // Assert
         $this->assertEquals($expected, $actual);
     }
+
+    /**
+     * Given 第一集買了 1 本
+     * And   第二集買了 1 本
+     * And   第三集買了 0 本
+     * And   第四集買了 0 本
+     * And   第五集買了 0 本
+     * When  結帳
+     * Then  價格應為 190 元
+     *
+     * @group ShoppingCartTest
+     * @group ShoppingCartTest1
+     */
+    public function testBuy_1_book1_and_1_book2_should_cost_190()
+    {
+        // Arrange
+        $this->target->addToCart(new Book('book1', 100));
+        $this->target->addToCart(new Book('book2', 100));
+        $expected = 190;
+
+        // Act
+        $actual = $this->target->checkOut();
+
+        // Assert
+        $this->assertEquals($expected, $actual);
+    }
 }
